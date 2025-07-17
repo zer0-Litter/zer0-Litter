22d663cf00a1a46131dcc815d11cb65180ea03c0
soomzi <ttn0a1@naver.com>
2025-07-17 11:06:55 +0900

    몽고디비 관련 models.mongo.py 업데이트 + urls.py추가

--- trash_loc/models.py
+++ trash_loc/models.py
@@ -1,2 +1,14 @@
+from django.db import models
+from common.models import TrashLoc
 
+class TrashLoc(models.Model):
+    t_lat = models.FloatField()
+    t_lon = models.FloatField()
+    t_road_addr = models.CharField(max_length=255)
+    t_detailed_addr = models.CharField(max_length=255, null=True, blank=True)
+    t_trash_type = models.CharField(max_length=100)
+    t_loc = models.CharField(max_length=255)
+    t_dept = models.CharField(max_length=100)
+    t_contact = models.CharField(max_length=100)
+    t_district_id = models.IntegerField()
  
