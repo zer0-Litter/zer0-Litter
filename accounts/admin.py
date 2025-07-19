b4ba238d28309563d5deb1ec34021b9251b1b3e3
tashapark <pdy2564@naver.com>
2025-07-19 17:35:11 +0900

    chore) :bug: :wrench: 슈퍼유저 생성을 위헤 Users 모델 birth 필드 nullable 처리 후 되돌림, 마이그레이션 2개 생성, trash_loc 뷰 변동 제거 및 url 주석처리

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from common.models import Users
 
-# Register your models here.
+@admin.register(Users)
+class CustomUserAdmin(UserAdmin):
+    model = Users
+    list_display = ('username', 'name', 'email', 'birth', 'gender', 'address', 'phone_number', 'user_reg_date', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        ("추가 정보", {'fields': ('name', 'birth', 'gender', 'address', 'phone_number', 'user_reg_date')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("추가 정보", {'fields': ('name', 'birth', 'gender', 'address', 'phone_number')}),
+    )
