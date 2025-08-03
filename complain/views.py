e5b2fd01d04a99cb8b96668e2b49fae775280143
tashapark <pdy2564@naver.com>
2025-08-03 16:17:51 +0900

    edit) :lipstick: nav ui, 비로그인 시 리다이렉트 수정, 로그인후 이전 페이지로 돌아가도록 아직 수정 필요 :poop:

--- complain/views.py
+++ complain/views.py
@@ -1,6 +1,6 @@
-
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='accounts:login')
 def complain_add(request):
-    return render(request, 'complain/complain_add.html')
+    return render(request, 'complain/complain_add.html')  # 템플릿 경로 맞게 수정
